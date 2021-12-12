05461544fa88bba76b770be76237ae60836c34c3
//Senora Developer <58949372+BetulNurGuner@users.noreply.github.com>
2021-12-12 04:04:15 +0300

    MainSayfasiAyarlandi

---
+++ main.js
@@ -0,0 +1,34 @@
+import { registerRootComponent } from 'expo';
+import {AppRegistry, Platform, StatusBar} from 'react-native';
+import {  createAppContainer } from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import App from './App';
+import Headlines from './Headlines';
+import NewsWebView from './NewsWebView';
+import {name as appName} from './app.json';
+
+const navigationConfig = {
+  initialRouteName: 'Home',
+  defaultNavigationOptions: {
+    title: 'Haber Kategorileri',
+    headerStyle: {
+      backgroundColor: '#2196F3',
+    },
+    headerTintColor: '#FFF',
+    headerTitleStyle: {
+      fontFamily: Platform.OS === 'ios' ? 'Futura' : 'Roboto',
+    },
+  },
+}
+ 
+StatusBar.setBarStyle('light-content', true);
+ 
+const MainNavigator = createStackNavigator({
+    Home: { screen: App },
+    Headlines: { screen: Headlines },
+    NewsWebView: { screen: NewsWebView },
+  },navigationConfig);
+
+  const MainContainer = createAppContainer(MainNavigator);
+
+registerRootComponent(MainContainer);
